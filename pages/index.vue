489a4b5298a6b219a042fcde31bf8633652deba6
“julio0901” <juliofernandez572@gmail.com>
2021-05-31 02:35:06 -0500

    utcancunestadias

---
+++ pages/index.vue
@@ -0,0 +1,515 @@
+
+<template>
+
+
+<div >
+<Figure>
+<img class=iconos src= "https://theboatplayadelcarmen.com/svg/retorno-de-la-inversion.svg">
+<img class=iconosDos src= "https://theboatplayadelcarmen.com/svg/sombrilla.svg">
+<img class=iconosTres src= "https://theboatplayadelcarmen.com/svg/diamante.svg">
+<img src="https://res.cloudinary.com/the-boat-playa-del-carmen/image/upload/f_auto,q_auto/v1615991966/the-boat/new-media/boat_portada-Marzo2021.webp" >
+
+<figcaption><img src="boat.png"></figcaption>
+<figcaption class="tresCirculos"><img src="https://res.cloudinary.com/the-boat-playa-del-carmen/image/upload/v1614873271/the-boat/new-media/CIRUCLOSboat_portada-Marzo202.png">
+</figcaption>
+
+<figcaption class="texBlanco">Real Capital Gains
+  <br>
+  <br>
+La Riviera Maya es y seguirá siendo la zona más 
+<br>
+codiciada del país debido a su riqueza arquelógica 
+<br>
+y sus playas espectaculares
+
+</figcaption>
+
+<figcaption class="texBlancoDos">Real Capital Gains
+  <br>
+  <br>
+La Riviera Maya es y seguirá siendo la zona más 
+<br>
+codiciada del país debido a su riqueza arquelógica 
+<br>
+y sus playas espectaculares
+
+</figcaption>
+
+<figcaption class="texBlancoTres">Real Capital Gains
+  <br>
+  <br>
+La Riviera Maya es y seguirá siendo la zona más 
+<br>
+codiciada del país debido a su riqueza arquelógica 
+<br>
+y sus playas espectaculares
+
+</figcaption>
+
+<figcaption class="bloqueNaranja">ENTREGA INMEDIATA
+<br>
+  <h2> Desde 147,500 USD* </h2> 
+
+<h1>FINANCIAMIENTO DIRECTO</h1>
+</figcaption>
+
+<h2 class="invitacion">La inversión que más gozarás en tu vida</h2>
+
+</Figure>
+
+<figure>
+
+<img src="https://res.cloudinary.com/the-boat-playa-del-carmen/image/upload/f_auto,q_auto:eco/v1599517025/the-boat/new-media/segunda-seccion.png">
+
+</figure>
+<figcaption class="texAzul">
+  <h2>
+  ¿Estás tan decepcionado con 
+  <br>
+  
+  tus inversiones que...?
+  </h2>
+<br>
+<br>
+*No las puedes disfrutar
+<br>
+*Sientes que se devalúan
+<br>
+*Son inseguras
+<br>
+*Tienen bajo rendimiento o solo rinden a largo plazo
+<br>
+<br>
+Invertir en bienes raíces dentro de la Riviera Maya, se consideracomo una inversión inteligente. Aunque los 
+<br>
+desarrolladores en pre-venta prometen precios accesibles, comprar en uno requiere mucha paciencia y
+<br>
+conlleva la incertidumbre de cuándo estará listo y si verdaderamente se terminará de construir.
+</figcaption>
+
+
+<figure>
+  <img src="marron.png">
+  <figcaption class="playaDelCarmen">
+
+<img src="https://res.cloudinary.com/the-boat-playa-del-carmen/image/upload/f_auto,q_auto:eco/v1599516991/the-boat/new-media/polaroid.png">
+  </figcaption>
+</figure>
+
+<figure>
+
+<img class="ubicacionExclusiva" src="https://res.cloudinary.com/the-boat-playa-del-carmen/image/upload/f_auto,q_auto:eco/v1601583877/the-boat/media/mapa-the-boat.jpg">
+<figcaption class="ubiEx">
+    <h2>Ubicación Exclusiva</h2>
+    <br>
+    Coco Beach. la zona más exclusiva de Playa del Carmen, a 100 mts de la playa y a tan solo 2 cuadras de la 5ta Av.
+</figcaption>
+</figure>
+
+
+<figure>
+<img src="https://mx.investmentpropertiesmexico.com/hs-fs/hubfs/marketing/developments/the-boat/the-boat-img-1.jpg?width=940&name=the-boat-img-1.jpg" >
+
+</figure>
+
+<figure>
+<img src="bote.jpg" >
+
+</figure>
+
+
+
+<figure>
+  <img src="fondo.png">
+  <figcaption>
+      <h1></h1> 
+  </figcaption>
+</figure>
+
+
+<figure>
+<img src="modelo.png" >
+
+<figcaption class="texModelo">
+
+  Fly & Buy
+
+</figcaption>
+
+<figcaption class="texModeloDos">
+
+  Playa del Carmen, Riviera Maya
+  <br>
+Aprovecha de nuestro programa durante 2021 donde te ofrecemos transportación gratuita aeropuerto / 
+<br>
+ condo-hotel / aeropuerto,y una noche de alojamiento en el paraíso, además de tomar 
+ <br>
+ a tu favor el costo del boleto de avión al comprar una unidad através de nosotros.
+
+</figcaption>
+
+<figcaption class="texModeloTres">
+
+  1 __ Mándanos tus datos y un asesor se 
+  <br>
+  pondra en contacto contigo..
+
+</figcaption>
+
+<figcaption class="texModeloCuatro">
+
+  
+2__Agenda la fecha de tu viaje este
+<br>
+ 2021, compra tus boletos de avión y 
+ <br>
+ viaja a Playa del Carmen.
+
+</figcaption>
+
+<figcaption class="texModeloQuinto">
+
+  3__Visita el desarollo, adquiere un
+  <br>
+   departamento y comienza a gozar tu
+   <br>
+    inversión inmediatamente.
+
+</figcaption>
+
+</figure>
+
+<figure>
+<img src="no.png" >
+
+</figure>
+
+<figure>
+<img src="playa.png" >
+<figcaption class="disfrutar">
+Después de este 2021... 🦠😷
+</figcaption>
+<figcaption class="disfrutarDos">
+  ¿Cuánto tiempo nos queda para disfrutar la vida?
+</figcaption>
+</figure>
+<figure>
+  <img src="iconos.png" alt="">
+  <figcaption class="formulario">
+    Aprovecha esta oportunidad de gozar tu inversión
+  </figcaption>
+  <form class="centro">
+    <label for="nombre">Nombre:</label>
+    <input type="text" id="nombre" placeholder="Escribe tu nombre">
+
+    <label for="telefono">Telefono:</label>
+    <input type="text" id="telefono" placeholder="Escribe tu telefono">
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" placeholder="Escribe tu Email">
+
+    <label for="mensaje">Mensaje: </label>
+    <textarea id="mensaje" placeholder="Escribe tu nombre"></textarea>
+
+    <input type="submit" value="Enviar">
+  </form>
+  
+</figure>
+
+<div >
+  <div >
+    <div class="w-full pt-12 flex flex-col sm:flex-row space-y-2 justify-start">
+      <div class="w-full sm:w-2/5 pr-6 flex flex-col space-y-4">
+
+      <img src="logoTheBoat.png">
+
+      
+      
+      </div>
+      <div class="w-full sm:w-1/5 flex flex-col space-y-4">
+        <a >¿NO ENCONTRASTE LO QUE BUSCAS?</a>
+        <a href="https://web.whatsapp.com/send?phone=+529841642844&text=Estoy%20interesado%20en%20recibir%20m%C3%A1s%20informaci%C3%B3n%20sobre%20The%20Boat%20Playa%20del%20Carmen">ayuda en linea</a>
+        <a>CONTACTA UN BROKER</a>
+        <a href="tel:+529841642844">Contactar</a>
+        <a >¿DUDAS SOBRE INVERSIÓN?</a>
+        <a href="tel:+529841642844">solicitar llamdas</a>
+      </div>
+
+       <div class="w-full sm:w-1/5 flex flex-col space-y-4">
+        <a >SIGUENOS EN </a>
+        <br>
+        <a>facebock</a>
+        <a href="https://www.facebook.com/theboatplayadelcarmen/">The Boat Playa del Carmen</a>
+        <br>
+        <a >instagram</a>
+        <a href="https://www.instagram.com/theboatplayadelcarmen/">theboatplayadelcarmen</a>
+      </div>
+      
+
+      
+      <div class="w-full sm:w-1/5 flex flex-col space-y-4">
+        <a >DATOS DE CONTACTO</a>
+        <a >Av. Calle 14 Nte 171, Gonzalo Guerrero, 77720 Playa del Carmen, Quintana Rooo</a>
+        <a href="tel:+529841642844">Tel: +52 984 164 28 44</a>
+        <br>
+        <a >HORARIO DE ATENCIÓN</a>
+        <a >Lunes - Viernes 9:00 am a 6:00 pm</a>
+      </div>
+      <div >
+        
+      </div>
+    </div>
+    <div class="footer text-white">
+      <center>
+      <p>©  2021 — The Boat Playa del Carmen.</p>
+      </center>
+    </div>
+  </div>
+</div>
+
+
+</div>
+</template>
+
+ <style >
+ .centro{
+    position: absolute;
+    left: 600px;
+    bottom: 10px;
+ }
+form{
+  width: 40%;
+  border: 0px solid #ccc;
+  margin: 10px;
+  padding: -20px;
+  
+}
+label{
+  font-size: 12px;
+  display: 500px;
+  width: 100%;
+}
+
+textarea{
+  resize: vertical;
+  max-height: 300px ;
+  min-height: 100px ;
+}
+
+input, textarea{
+  margin-bottom: 20px;
+  width: 100%;
+  padding: 10px ;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  border: 1px solid #ccc; 
+}
+
+input:focus, textarea:focus{
+  border: 1px solid #1668c4;
+}
+
+input[type="submit"]{
+margin-bottom: 0px;
+background: #1668c4 ;
+color: white;
+border: none;
+}
+input[type="submit"]:hover{
+background: #4a88f4;
+}
+
+.formulario{
+position: absolute;
+    left: 500px;
+    bottom: 700px;
+    font-size: 40px;
+}
+.footer{
+    text-align:center;
+    position: relative;
+    top: 50px;
+    background: #2e2e2e;
+    width: 100%;
+    height: 50px;
+            }
+  figure{
+    margin: 0;
+    display: inline-block;
+    border: 0px;
+    position: relative;
+  }
+
+  figcaption{
+  
+    position: absolute;
+    left: -101px;
+    bottom: 1200px;
+  }
+  .tresCirculos{
+    position: absolute;
+    left: 1100px;
+    bottom: 500px;
+  }
+  .icon{
+    position: absolute;
+    height: 100px;
+    width: 30%;
+  }
+  .iconos{
+    position: absolute;
+    left: -50px;
+    bottom: 200px;
+    height: 100px;
+    width: 30%;
+  }
+  .iconosDos{
+    position: absolute;
+    left: 510px;
+    bottom: 200px;
+    height: 100px;
+    width: 30%;
+  }
+  .iconosTres{
+    position: absolute;
+    left: 1070px;
+    bottom: 200px;
+    height: 100px;
+    width: 30%;
+  }
+.playaDelCarmen{
+    position: absolute;
+    left: 1100px;
+    bottom: 500px;
+}
+.ubicacionExclusiva{
+    left: 2100px;
+    bottom: 500px;
+    height: 500px;
+    width: 50%;
+  
+}
+.ubiEx{
+  position: absolute;
+    left: 1100px;
+    bottom: 100px;
+}
+
+
+
+.banner {
+  height: 10; 
+  width: 10000px;
+  margin: 0;
+  padding: 0;
+  }
+
+.bloqueNaranja{
+    background: orangered;
+    position: absolute;
+    left: 101px;
+    bottom: 1150px;
+    color: white;
+    font-size: 30px;
+
+  }
+
+.texBlanco{
+    color: white;
+    position: absolute;
+    left: 300px;
+    bottom: 200px;
+  }
+  .texModelo{
+    color: white;
+    position: absolute;
+    left: 300px;
+    bottom: 500px;
+    font-size: 90px;
+  }
+  .texModeloDos{
+    color: white;
+    position: absolute;
+    left: 300px;
+    bottom: 370px;
+    font-size: 20px;
+  }
+
+  .texModeloTres{
+    color: white;
+    position: absolute;
+    left: 300px;
+    bottom: 170px;
+    font-size: 20px;
+  }
+  .texModeloCuatro{
+    color: white;
+    position: absolute;
+    left: 700px;
+    bottom: 150px;
+    font-size: 20px;
+  }
+
+  .texModeloQuinto{
+    color: white;
+    position: absolute;
+    left: 1100px;
+    bottom: 150px;
+    font-size: 20px;
+  }
+
+  .disfrutar{
+    color: black;
+    position: absolute;
+    left: 780px;
+    bottom: 310px;
+    font-size: 25px;
+  }
+  .disfrutarDos{
+    color: black;
+    position: absolute;
+    left: 550px;
+    bottom: 210px;
+    font-size: 35px;
+  }
+
+  .texBlancoDos{
+    color: white;
+    position: absolute;
+    left: 850px;
+    bottom: 200px;
+  }
+    .texBlancoTres{
+    color: white;
+    position: absolute;
+    left: 1410px;
+    bottom: 200px;
+  }
+
+  .texAzul{
+    color: blue;
+    position: absolute;
+    left: 900px;
+    bottom: -1050px;
+    
+  }
+  .invitacion{
+    position: absolute;
+    left: 100px;
+    bottom: 600px;
+    color: white;
+    font-size: 70px;
+  }
+  
+  h1{
+    font-size: 20px;
+  }
+  h2{
+    font-size: 40px;
+  }
+   
+
+  
+   </style>
+
